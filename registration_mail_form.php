cebbc1906c0c752960b87bfad5a8f83a868c7022
MinelAnthem <42905182+MinelAnthem@users.noreply.github.com>
2019-01-21 22:41:13 +0900

    Update registration_mail_form.php
    コメント文削除

--- registration_mail_form.php
+++ registration_mail_form.php
@@ -3,11 +3,6 @@ session_start();
 
 header("Content-type: text/html; charset=utf-8");
 
-/*
-//クロスサイトリクエストフォージェリ対策
-$_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
-$token = $_SESSION['token'];
-*/
 //クリックジャッキング対策
 header('X-FRAME-OPTIONS: SAMEORIGIN');
 
